477c5c1872ffd9ad253c233b00d2ea608a25cbc2
IMAN <Imannursoleh610@gmail.com>
2023-10-24 13:55:15 +0700

    Initial commit

--- app.py
+++ app.py
@@ -3,24 +3,15 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 
-# Load environment variables from .env file
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-# Create the Flask app
 app = Flask(__name__)
 
-# Define a route for the home page
 @app.route('/', methods=['GET'])
 def home():
     return render_template('porto.html')
-
-# Define a route for the index page
-@app.route('/index', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-# Define a route to get information
+ 
 @app.route('/info', methods=['GET'])
 def get_info():
     my_name = request.args.get('my_name')
